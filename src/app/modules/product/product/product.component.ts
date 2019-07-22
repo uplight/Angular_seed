b1f386fb542122e8bcc48b3de5b3140e0cb4327f
vladt <vladstitov@gmail.com>
2019-07-22 18:28:02 -0400

    animations

--- src/app/modules/product/product/product.component.ts
+++ src/app/modules/product/product/product.component.ts
@@ -2,11 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {ProductService} from '@product/services/product.service';
 import {VOProduct} from '@app/core/models/products';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
+  animations: [
+    // Each unique animation requires its own trigger. The first argument of the trigger function is the name
+    trigger('rotatedState', [
+      state('default', style({ transform: 'rotate(0)' })),
+      state('rotated', style({ transform: 'rotate(-180deg)' })),
+      transition('rotated => default', animate('1500ms ease-out')),
+      transition('default => rotated', animate('400ms ease-in'))
+    ])
+  ]
 })
 export class ProductComponent implements OnInit {
 
